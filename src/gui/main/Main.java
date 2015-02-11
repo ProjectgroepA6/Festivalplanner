537b1fcf467b648c5c6a1a9442c2dd2983f0cf99
Gilian Joosen <gilian_joosen@hotmail.com>
2015-02-11 16:32:54 +0100

    Added menu bar support for OSX.

--- src/gui/main/Main.java
+++ src/gui/main/Main.java
@@ -10,8 +10,16 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class Main {
 	
 	public static void main(String[] args){
-		//sets the systems look and feels
-		try{
+        
+		//sets the systems look and feels 
+        
+        //osx
+        if(System.getProperties().getProperty("os.name").equals("Mac OS X")) {
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
+        }
+
+        try{
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}catch(ClassNotFoundException | InstantiationException| IllegalAccessException | UnsupportedLookAndFeelException e) {
 			e.printStackTrace();
